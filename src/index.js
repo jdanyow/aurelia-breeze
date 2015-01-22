451afcdc8cc6020a8777237208f8bd6d8938cce1
Jeremy Danyow <jdanyow@gmail.com>
2015-01-22 14:22:51 -0500

    Modeled after Aurelia OoObjectObserver and OoPropertyObserver

--- src/index.js
+++ src/index.js
@@ -1,82 +1,110 @@
-export class BreezeScalarPropertyObserver {
-  constructor(object, propertyName) {
-    this.object = object;
-    this.propertyName = propertyName;
+import {ObjectObservationAdapter} from 'aurelia-binding';
+
+export class BreezeObjectObserver {
+  constructor(obj){
+    this.obj = obj;
+    this.observers = {};  
   }
 
-  getValue() {
-    return this.object[this.propertyName];
-  }
-
-  setValue(value) {
-    this.object[this.propertyName] = value;
-  }
-
-  subscribe(callback) {
-    // lazy create the callbacks array.
-    if (!this.callbacks)
-      this.callbacks = [];
-    let callbacks = this.callbacks;
-
-    // push the callback.
-    // todo: ask Rob if we need to check whether a callback has already been added.
+  subscribe(propertyObserver, callback){
+    var callbacks = propertyObserver.callbacks;
     callbacks.push(callback);
 
-    // todo: ask Ward about using entityManager.entityChanged to reduce number of subscriptions (if that even matters).  need to consider detached entities.
-    if (!this.hasOwnProperty('propertyChangedSubscription')) {
-      this.propertyChangedSubscription = this.object.entityAspect.propertyChanged.subscribe(
-        function(entity, property, propertyName, oldValue, newValue, parent) {
-          for (var i = 0; i < callbacks.length; i++) {
-            callbacks[i](); // todo: what are the callback args?
-          }
+    if(!this.observing){
+      this.observing = true;
+      this.subscription = this.obj.entityAspect.propertyChanged.subscribe(
+        (entity, property, propertyName, oldValue, newValue, parent) => {
+          this.handleChanges([{name: propertyName, object: entity, type: 'update', oldValue: oldValue}]);
         });
     }
 
-    return () => {
-      // dispose.
-      callbacks.splice(callbacks.indexof(callback), 1);
+    return function(){      
+      callbacks.splice(callbacks.indexOf(callback), 1);
       if (callbacks.length > 0)
         return;
-      this.object.entityAspect.propertyChanged.unsubscribe(this.propertyChangedSubscription);
+      this.obj.entityAspect.propertyChanged.unsubscribe(this.subscription);
+      this.observing = false;
+    };
+  }
+
+  getObserver(propertyName){
+    var propertyObserver = this.observers[propertyName]
+      || (this.observers[propertyName] = new BreezePropertyObserver(this, this.obj, propertyName));
+
+    return propertyObserver;
+  }
+
+  handleChanges(changeRecords){
+    var updates = {},
+        observers = this.observers,
+        i = changeRecords.length;
+    
+    while(i--) {
+      var change = changeRecords[i],
+          name = change.name;
+  
+      if(!(name in updates)){
+        var observer = observers[name];
+        updates[name] = true;
+        if(observer){
+          observer.trigger(change.object[name], change.oldValue);
+        }
+      }
     }
   }
 }
 
-export class BreezeNonScalarPropertyObserver {
-  // todo: ask Rob what the callbacks should be invoked with when items are added/removed etc etc.
-  // non-scalar nav props shouldn't be assignable right?
+export class BreezePropertyObserver {
+  constructor(owner, obj, propertyName){
+    this.owner = owner;
+    this.obj = obj;
+    this.propertyName = propertyName;
+    this.callbacks = [];
+    this.isSVG = false;
+  }
+
+  getValue(){
+    return this.obj[this.propertyName];
+  }
+
+  setValue(newValue){
+    this.obj[this.propertyName] = newValue;
+  }
+
+  trigger(newValue, oldValue){
+    var callbacks = this.callbacks,
+        i = callbacks.length;
+
+    while(i--) {
+      callbacks[i](newValue, oldValue);
+    }
+  }
+
+  subscribe(callback){
+    return this.owner.subscribe(this, callback);
+  }
 }
 
-export class BreezeBindingAdapter {
+export class BreezeObservationAdapter {
   handlesProperty(object, propertyName) {
-    // to discuss with Ward:  probably a hot path...
+    // probably a hot path...
     // todo: compare perf of entityType.getProperty vs searching entityType.dataProperties/complexProperties.
     // todo: consider keeping dictionary-object keyed on entityType.name + propertyName to avoid reflecting 
     //       on the breeze type each time.  would need a way to scope the dictionary to a particular 
     //       metadatastore instance or resource name...
-    // todo: consider support for binding to props on the entityAspect that have events such as "validationErrors".
     // todo: test whether change events are raised for detached entities.
 
-    let entityType = object.entityType;
-    return entityType && object.entityAspect && entityType.getProperty(propertyName);
+    var entityType = object.entityType, property;
+    return entityType && object.entityAspect && (property = entityType.getProperty(propertyName)) && property.isScalar;
   }
 
   createObserver(object, propertyName) {
-    if (object.entityType.getProperty(propertyName).isScalar)
-      return new BreezeScalarPropertyObserver(object, propertyName);
-
-    // todo:
-    throw new Error('Not Implemented:  non-scalar data properties and navigation properties');
+    if (!handlesProperty(object, propertyName))
+      throw new Error(`BreezeBindingAdapter does not support observing the ${propertyName} property.  Check the handlesProperty method before calling createObserver.`);
+    // todo: return the new observer... need to add properties similar to Aurelia's __observers__ and _observer_ properties
   }
 }
 
-
-
-// todo: add tests for all combinations of:
-
-// complex types
-// enttiy types
-// scalar data properties
-// non-scalar data properties
-// scalar navigation properties
-// non-scalar navigation properties
+export function install(aurelia) {
+  aurelia.withInstance(ObjectObservationAdapter, new BreezeObservationdapter());
+}
